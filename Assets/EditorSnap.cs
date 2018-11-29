baa6b8a7ad90c4574b43bd3086445fff7df1e56d
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-11-29 21:04:49 +0200

    RR01 [ExecuteInEditMode] attribute

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
+    }
+}
