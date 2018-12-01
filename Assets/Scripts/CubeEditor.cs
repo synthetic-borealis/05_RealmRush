6a99a3e46169ee7d1ab998bea74587fa81fadae2
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-12-01 16:08:27 +0200

    RR03 Using TextMesh for labels

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
 
-    void Awake()
+    TextMesh textMesh;
+
+    private void Start()
     {
         
     }
 
-    void Update()
+    private void Update()
     {
         Vector3 snapPosition;
+        
         snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPosition.x / gridSize + "," + snapPosition.z / gridSize;
     }
 }
