eb35cff686de296a16272a8a9e16b2f17e33b390
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-12-04 00:35:24 +0200

    RR08 The dictionary data structure

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+	// Use this for initialization
+	void Start()
+    {
+        LoadBlocks();
+	}
+
+    private void LoadBlocks()
+    {
+        Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            bool isOverlapping = grid.ContainsKey(waypoint.GetGridPos());
+            if (isOverlapping)
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+        }
+        print("Loaded " + grid.Count + " blocks");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+	}
+}
