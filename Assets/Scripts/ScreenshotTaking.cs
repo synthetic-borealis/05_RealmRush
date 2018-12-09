e3f17696df6c8ec827b9b17b4d0a12fd7596c415
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-12-09 23:07:17 +0200

    RR14 Adding production assets

---
+++ Assets/Scripts/ScreenshotTaking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotTaking : MonoBehaviour
+{
+    [SerializeField] KeyCode screenshotKey = KeyCode.C;
+    int screenshotIndex = 0;
+	
+	// Update is called once per frame
+	void Update()
+    {
+		if (Input.GetKeyDown(screenshotKey))
+        {
+            if (System.IO.File.Exists("Screenshot(" + screenshotIndex + ").png"))
+            {
+                screenshotIndex++;
+            }
+            ScreenCapture.CaptureScreenshot("Screenshot(" + screenshotIndex + ").png");
+            screenshotIndex++;
+        }
+	}
+}
