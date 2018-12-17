92d072cf6ff303600e27dfe47d05ab5e2e787165
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-12-17 20:16:30 +0200

    RR20 Revise coroutine & much more (added basic enemy spawner)

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] EnemyMovement enemyPrefab;
+    [SerializeField] float secondsBetweenSpawning = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true) // loop forever
+        {
+            EnemyMovement newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            newEnemy.transform.parent = transform;
+            yield return new WaitForSeconds(secondsBetweenSpawning);
+        }
+    }
+}
