31a647aacb174a28b9cad1b9bde09e97e89bfe51
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-12-23 21:27:38 +0200

    RR26 Implementing a ring buffer (or not)

---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour
+{
+    [SerializeField] int towerLimit = 5;
+    [SerializeField] Tower towerPrefab;
+    private int numberOfTowers = 0;
+
+    public void AddTower(Waypoint baseWaypoint)
+    {
+        if (numberOfTowers < towerLimit)
+        {
+            InstantiateNewTower(baseWaypoint);
+            numberOfTowers++;
+        }
+        else
+        {
+            MoveExistingTower(baseWaypoint);
+        }
+    }
+
+    private static void MoveExistingTower(Waypoint baseWaypoint)
+    {
+        print("Can`t place more towers!");
+    }
+
+    private void InstantiateNewTower(Waypoint baseWaypoint)
+    {
+        Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
+        baseWaypoint.isPlaceable = false;
+    }
+}
