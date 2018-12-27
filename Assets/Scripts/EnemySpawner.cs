a3996a6708bea45cebb2ffe2a46d1e243a34e734
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-12-27 20:41:24 +0200

    RR31 Display health & score

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] EnemyMovement enemyPrefab;
     [Range(0.1f, 120f)] [SerializeField] float secondsBetweenSpawning = 5f;
     [SerializeField] Transform deathFXParentTransform;
+    [SerializeField] Text spawnedEnemies;
+    int score = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +21,18 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true) // loop forever
         {
+            AddScore();
             EnemyMovement newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             newEnemy.transform.parent = transform;
             newEnemy.GetComponent<EnemyDamage>().deathFXParentTransform = deathFXParentTransform;
             newEnemy.GetComponent<EnemyMovement>().deathFXParentTransform = deathFXParentTransform;
             yield return new WaitForSeconds(secondsBetweenSpawning);
         }
+    }
+
+    private void AddScore()
+    {
+        score++;
+        spawnedEnemies.text = score.ToString();
     }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int health = 10;
     [SerializeField] int healthDecrease = 1;
+    [SerializeField] Text healthText;
+
+    private void Start()
+    {
+        healthText.text = health.ToString();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (health > 0)
         {
             health -= healthDecrease;
-        }
-        else
-        {
-            // Die
+            healthText.text = health.ToString();
         }
     }
 }
