34215f3a71a6f3f99597a5ce1c46c2c4fd807ba0
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-12-27 17:41:17 +0200

    RR30 Health for base

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (health > 0)
+        {
+            health -= healthDecrease;
+        }
+        else
+        {
+            // Die
+        }
+    }
+}
